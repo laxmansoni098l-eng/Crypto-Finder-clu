0d8312fcca9e42d3e706739824f101fb745191cc
Laxman Soni <laxmansoni098l@gmail.com>
2025-10-18 11:50:02 +0000

    Puri website ka font style change kro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from 'next';
 import { Toaster } from "@/components/ui/toaster";
 import './globals.css';
+import { Space_Grotesk } from 'next/font/google';
+
+const spaceGrotesk = Space_Grotesk({ 
+  subsets: ['latin'],
+  variable: '--font-space-grotesk'
+});
 
 export const metadata: Metadata = {
   title: 'Crypto Finder Club | The Best Launchpad',
@@ -13,12 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
+    <html lang="en" className={`${spaceGrotesk.variable} dark`}>
       <body className="font-body antialiased bg-background text-foreground">
         {children}
         <Toaster />
