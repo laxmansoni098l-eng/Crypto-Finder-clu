78b7e7a3ebd071320deb60060441ed81bfe24f9c
Laxman Soni <laxmansoni098l@gmail.com>
2025-10-21 07:51:30 +0000

    Kya kr rhe ho abhi tak add nhi hua

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,7 +8,7 @@ export function Logo() {
         alt="Crypto Finder Club Logo"
         width={40}
         height={40}
-        className="rounded-md border-2 border-primary/50"
+        className="rounded-md border-2 border-primary/50 aspect-square"
       />
       <div className="flex flex-col">
         <span className="font-bold uppercase tracking-widest text-lg text-foreground">
