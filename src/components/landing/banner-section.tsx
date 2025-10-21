c348247187dc2ab1009b05c4a5f363aa01942d55
Laxman Soni <laxmansoni098l@gmail.com>
2025-10-21 07:15:36 +0000

    Good  add this <a href='https://postimages.org/' target='_blank'><img sr

--- src/components/landing/banner-section.tsx
+++ src/components/landing/banner-section.tsx
@@ -7,10 +7,17 @@ export function BannerSection() {
         <h2 className="text-3xl md:text-5xl font-bold text-center mb-12 uppercase text-foreground">
           PREVIOUS <span className="text-primary">WORK</span>
         </h2>
-        <div className="flex justify-center items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 justify-items-center items-center">
             <Image
               src="https://i.postimg.cc/63NXQhTz/IMG-20251021-123741-831.jpg"
               alt="Previous work banner"
+              width={600}
+              height={300}
+              className="object-contain rounded-lg"
+            />
+            <Image
+              src="https://i.postimg.cc/Pxzkv88Q/IMG-20251021-123742-104.jpg"
+              alt="Previous work banner 2"
               width={600}
               height={300}
               className="object-contain rounded-lg"
