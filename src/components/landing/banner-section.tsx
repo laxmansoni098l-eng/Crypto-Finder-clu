fc61e4b62222fa629519278ff572427d40c6dc69
Laxman Soni <laxmansoni098l@gmail.com>
2025-10-21 07:21:46 +0000

    Ye automatic slide hone chahiye or Inka ek frame me add kro

--- src/components/landing/banner-section.tsx
+++ src/components/landing/banner-section.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Image from "next/image";
+import * as React from "react";
+import Autoplay from "embla-carousel-autoplay";
 import {
   Carousel,
   CarouselContent,
@@ -8,41 +12,45 @@ import {
 } from "@/components/ui/carousel";
 
 const banners = [
-    {
-      src: "https://i.postimg.cc/63NXQhTz/IMG-20251021-123741-831.jpg",
-      alt: "Previous work banner",
-    },
-    {
-      src: "https://i.postimg.cc/Pxzkv88Q/IMG-20251021-123742-104.jpg",
-      alt: "Previous work banner 2",
-    },
-    {
-      src: "https://i.postimg.cc/mDGsrShq/IMG-20251021-123742-729.jpg",
-      alt: "Previous work banner 3",
-    },
-    {
-      src: "https://i.postimg.cc/2yRMGjnL/IMG-20251021-123741-896.jpg",
-      alt: "Previous work banner 4",
-    },
-    {
-      src: "https://i.postimg.cc/pTtHdByS/IMG-20251021-123742-208.jpg",
-      alt: "Previous work banner 5",
-    },
-    {
-      src: "https://i.postimg.cc/yxLhdv6d/IMG-20251021-123742-347.jpg",
-      alt: "Previous work banner 6",
-    },
-    {
-      src: "https://i.postimg.cc/j5QrnJJh/IMG-20251021-123742-465.jpg",
-      alt: "Previous work banner 7",
-    },
-    {
-      src: "https://i.postimg.cc/j5QrnJJG/IMG-20251021-123742-736.jpg",
-      alt: "Previous work banner 8",
-    },
-]
+  {
+    src: "https://i.postimg.cc/63NXQhTz/IMG-20251021-123741-831.jpg",
+    alt: "Previous work banner",
+  },
+  {
+    src: "https://i.postimg.cc/Pxzkv88Q/IMG-20251021-123742-104.jpg",
+    alt: "Previous work banner 2",
+  },
+  {
+    src: "https://i.postimg.cc/mDGsrShq/IMG-20251021-123742-729.jpg",
+    alt: "Previous work banner 3",
+  },
+  {
+    src: "https://i.postimg.cc/2yRMGjnL/IMG-20251021-123741-896.jpg",
+    alt: "Previous work banner 4",
+  },
+  {
+    src: "https://i.postimg.cc/pTtHdByS/IMG-20251021-123742-208.jpg",
+    alt: "Previous work banner 5",
+  },
+  {
+    src: "https://i.postimg.cc/yxLhdv6d/IMG-20251021-123742-347.jpg",
+    alt: "Previous work banner 6",
+  },
+  {
+    src: "https://i.postimg.cc/j5QrnJJh/IMG-20251021-123742-465.jpg",
+    alt: "Previous work banner 7",
+  },
+  {
+    src: "https://i.postimg.cc/j5QrnJJG/IMG-20251021-123742-736.jpg",
+    alt: "Previous work banner 8",
+  },
+];
 
 export function BannerSection() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  );
+
   return (
     <section className="py-20">
       <div className="container mx-auto px-4">
@@ -50,22 +58,27 @@ export function BannerSection() {
           PREVIOUS <span className="text-primary">WORK</span>
         </h2>
         <Carousel
+          plugins={[plugin.current]}
           opts={{
             align: "start",
             loop: true,
           }}
+          onMouseEnter={plugin.current.stop}
+          onMouseLeave={plugin.current.reset}
           className="w-full max-w-4xl mx-auto"
         >
           <CarouselContent>
             {banners.map((banner, index) => (
               <CarouselItem key={index} className="flex justify-center">
-                <Image
-                  src={banner.src}
-                  alt={banner.alt}
-                  width={800}
-                  height={400}
-                  className="object-contain rounded-lg"
-                />
+                <div className="p-2 bg-card rounded-xl border-2 border-primary/20 shadow-lg">
+                  <Image
+                    src={banner.src}
+                    alt={banner.alt}
+                    width={800}
+                    height={400}
+                    className="object-contain rounded-lg"
+                  />
+                </div>
               </CarouselItem>
             ))}
           </CarouselContent>
