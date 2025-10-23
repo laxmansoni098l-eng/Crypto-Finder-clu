d078e387d272b5580ec84b1b66ea8a83a24d7cbf
Laxman Soni <laxmansoni098l@gmail.com>
2025-10-23 03:43:41 +0000

    Yaar nhi ho rha hai mujhe kuchh Krna hai toh mujhe btao ise jldi fix kro

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
 
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from '@/components/ui/toaster';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
